9a6265f2f769f95f310564cd35a8183a1d964655
Bob Yantosca <yantosca@seas.harvard.edu>
2022-02-10 14:05:29 -0500

    Split source code into src and test folders
    We now have split the QFYAML source code into the following folders:
    
    src/
    - Contains the main qfyaml_mod.F90 source code
    
    test/
    - Contains testing programs:
      (1) test_qfyaml.F90 and input file input.yml
      (2) test_geoschem_config.F90 and input file geoschem_config.yml
      (3) test_species_database.F90 and input files species_database*.yml
    
    Also updated the CMakeLists and cmake/GNU.cmake and cmake/Intel.cmake
    files accordingly.
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- cmake/GNU.cmake
+++ cmake/GNU.cmake
@@ -1,14 +1,18 @@
-# Compiler specific flags for GNU Fortran compiler
-# NOTE: Flags are taken from the GEOS-Chem build sequence
+#### Specific flags for GNU Fortran compiler
 
-# Base set of compiler flags
-set(CMAKE_Fortran_FLAGS "-g -O0 -cpp -fbacktrace -ffree-line-length-512 -w -fautomatic -fno-align-commons -fno-range-check -mcmodel=medium -std=legacy")
+# Base set of GNU compiler flags
+set(CMAKE_Fortran_FLAGS_GNU
+  -g -O0 -cpp -fbacktrace -ffree-line-length-512 -w -fautomatic
+  -fno-align-commons -fno-range-check -mcmodel=medium -std=legacy
+)
 
-# Additional compiler flags for Release
-set(CMAKE_Fortran_FLAGS_RELEASE "-O3")
+# Additional GNI compiler flags for Release
+set(CMAKE_Fortran_FLAGS_RELEASE_GNU
+  -O3
+)
 
-# Additional compiler flags for Debug
-set(CMAKE_Fortran_FLAGS_DEBUG  "-O0 -gdwarf-2 -gstrict-dwarf -O0 -Wall -Wextra -Wconversion -Warray-temporaries -fcheck-array-temporaries")
-
-# Set a cpp switch to denote GNU
-add_definitions(-D_GNU)
+# Additional GNU compiler flags for Debug
+set(CMAKE_Fortran_FLAGS_DEBUG_GNU
+  -O0 -gdwarf-2 -gstrict-dwarf -O0 -Wall -Wextra -Wconversion
+  -Warray-temporaries -fcheck-array-temporaries
+)
